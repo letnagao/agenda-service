8761ce38c46b7c0debdd37b2ec7f2e43b7a8f3e5
letnagao <leticia-nagao@hotmail.com>
2022-08-11 12:22:40 -0300

    added values in paciante

---
+++ src/main/java/pdev/com/agenda/domain/service/PacienteService.java
@@ -0,0 +1,36 @@
+package pdev.com.agenda.domain.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pdev.com.agenda.domain.entity.Paciente;
+import pdev.com.agenda.domain.repository.PacienteRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PacienteService {
+
+    private final PacienteRepository repository;
+
+    public Paciente salvar(Paciente paciente) {
+
+        return repository.save(paciente);
+    }
+
+    public List<Paciente> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Optional<Paciente> buscarPorId(Long id) {
+        return repository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+
+}
